045d9964d52966f7c1d9a0c3b7d6ddc5e54da90e
xXTheBigGunXx <144613981+xXTheBigGunXx@users.noreply.github.com>
2025-07-19 23:43:52 +0300

    Set up of Chess.com API

--- frontend/src/Components/SearchBar.tsx
+++ frontend/src/Components/SearchBar.tsx
@@ -4,19 +4,25 @@ import useDebounce from '../Hooks/useDebounce';
 import { DEBOUNCE_DELAY } from '../Utils/constants';
 import { chessPlayerAPI } from '../API/chessPlayerApi';
 
+import { useDispatch } from 'react-redux';
+import { setPhase } from '../Store/appSlice';
+
 export const SearchBar = () => {
 
     const [username, setUsername] = useState('');
     const debounceSearchTerm = useDebounce(username, DEBOUNCE_DELAY);
 
+    const dispatch = useDispatch();
+
     useEffect(() => {
-        console.log();
         const getRatings = async () => {
             const response = await chessPlayerAPI(username);
             console.log(response);
             
         }
+        dispatch(setPhase('LOADING'));
         getRatings();
+        dispatch(setPhase('BOARD'));
     }, [debounceSearchTerm]);
 
     return (
